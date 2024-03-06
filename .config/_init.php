e89bf967fbb8519c1b2a6d352b88950769c88fd7
Oryvex <130756455+Oryvex@users.noreply.github.com>
2024-03-06 17:12:04 +0530

    Create _init.php

---
+++ .config/_init.php
@@ -0,0 +1,7 @@
+<?php 
+$rootPath = $_SERVER['DOCUMENT_ROOT'];
+
+require_once($rootPath . '/serve/Router.php');
+require_once($rootPath . '/serve/Source.php');
+require_once($rootPath . '/serve/Apiconn.php');
+?>
